9992f40d0a2dc30b9ce21be827de11ca4c39cd04
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-17 23:04:10 +0530

    Fix Lint

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ import logging
 import os
 
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
