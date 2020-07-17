4ac1b68cffbcc4c2314e488a70ba386d86363c85
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-17 22:54:43 +0530

    Update gitignore

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,16 @@ import logging
 import os
 
 from flask import Flask
-
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return os.system("python3 predict.py --checkpoint='./checkpoint.pth' --image='#from-post-request' ")
+    return 'Hello World!'
+    # return os.system("python3 predict.py --checkpoint='./checkpoint.pth' --image='#from-post-request' ")
 
 
 if __name__ == '__main__':
