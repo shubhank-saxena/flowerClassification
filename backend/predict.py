ab4369977bd470ba7b51df40ffa7930f2b3a6573
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-19 16:52:41 +0000

    Modify model training Shift to resnet

--- backend/predict.py
+++ backend/predict.py
@@ -1,174 +1,107 @@
-import argparse
-import json
-import PIL
 import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
 import numpy as np
+from collections import OrderedDict
+from PIL import Image
 
-from math import ceil
-from torchvision import models
-
-
-def arg_parser():
-    # Define a parser
-    parser = argparse.ArgumentParser(description="Neural Network Settings")
-
-    # Point towards image for prediction
-    parser.add_argument('--image', type=str, help='Point to impage file for prediction.', required=True)
-
-    # Load checkpoint created by train.py
-    parser.add_argument('--checkpoint', type=str, help='Point to checkpoint file as str.', required=True)
-
-    # Specify top-k
-    parser.add_argument('--top_k', type=int, help='Choose top K matches as int.')
-
-    # Import category names
-    parser.add_argument('--category_names', type=str, help='Mapping from categories to real names.')
-
-    # Add GPU Option to parser
-    parser.add_argument('--gpu', action="store_true", help='Use GPU + Cuda for calculations')
-
-    # Parse args
-    args = parser.parse_args()
-
-    return args
-
-
-# Function load_checkpoint(checkpoint_path) loads our saved deep learning model from checkpoint
-def load_checkpoint(checkpoint_path):
-    # Load the saved file
-    checkpoint = torch.load("checkpoint.pth", map_location=torch.device('cpu'))
-
-    # Load Defaults if none specified
-    if checkpoint['architecture'] == 'vgg16':
-        model = models.vgg16(pretrained=True)
-        model.name = "vgg16"
-    else:
-        exec("model = models.{%s}(pretrained=True)", checkpoint['architecture'])
-        model.name = checkpoint['architecture']
-
-    # Freeze parameters so we don't backprop through them
-    for param in model.parameters():
-        param.requires_grad = False
-
-    # Load stuff from checkpoint
-    model.class_to_idx = checkpoint['class_to_idx']
-    model.classifier = checkpoint['classifier']
-    model.load_state_dict(checkpoint['state_dict'])
-
+def loading_model (file_path):
+    checkpoint = torch.load (file_path) #loading checkpoint from a file
+    model = models.alexnet (pretrained = True) #function works solely for Alexnet
+    #you can use the arch from the checkpoint and choose the model architecture in a more generic way:
+    #model = getattr(models, checkpoint['arch']
+        
+    model.classifier = checkpoint ['classifier']
+    model.load_state_dict (checkpoint ['state_dict'])
+    model.class_to_idx = checkpoint ['mapping']
+    
+    for param in model.parameters(): 
+        param.requires_grad = False #turning off tuning of the model
+    
     return model
 
+model_verify = loading_model ('project_checkpoint.pth')
 
-# Function process_image(image_path) performs cropping, scaling of image for our model
-def process_image(image_path):
-    test_image = PIL.Image.open(image_path)
-
-    # Get original dimensions
-    orig_width, orig_height = test_image.size
-
-    # Find shorter size and create settings to crop shortest side to 256
-    if orig_width < orig_height:
-        resize_size = [256, 256 ** 600]
-    else:
-        resize_size = [256 ** 600, 256]
-
-    test_image.thumbnail(size=resize_size)
-
-    # Find pixels to crop on to create 224x224 image
-    center = orig_width / 4, orig_height / 4
-    left, top, right, bottom = center[0] - (244 / 2), center[1] - (244 / 2), center[0] + (244 / 2), center[1] + (244 / 2)
-    test_image = test_image.crop((left, top, right, bottom))
-
-    # Converrt to numpy - 244x244 image w/ 3 channels (RGB)
-    np_image = np.array(test_image) / 255  # Divided by 255 because imshow() expects integers (0:1)!!
-
-    # Normalize each color channel
-    normalise_means = [0.485, 0.456, 0.406]
-    normalise_std = [0.229, 0.224, 0.225]
-    np_image = (np_image - normalise_means) / normalise_std
-
-    # Set the color to the first channel
-    np_image = np_image.transpose(2, 0, 1)
-
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    #size = 256, 256
+    im = Image.open (image) #loading image
+    width, height = im.size #original size
+    #proportion = width/ float (height) #to keep aspect ratio
+    
+    if width > height: 
+        height = 256
+        im.thumbnail ((50000, height), Image.ANTIALIAS)
+    else: 
+        width = 256
+        im.thumbnail ((width,50000), Image.ANTIALIAS)
+        
+    
+    width, height = im.size #new size of im
+    #crop 224x224 in the center
+    reduce = 224
+    left = (width - reduce)/2 
+    top = (height - reduce)/2
+    right = left + 224 
+    bottom = top + 224
+    im = im.crop ((left, top, right, bottom))
+    
+    #preparing numpy array
+    np_image = np.array (im)/255 #to make values from 0 to 1
+    np_image -= np.array ([0.485, 0.456, 0.406]) 
+    np_image /= np.array ([0.229, 0.224, 0.225])
+    
+    np_image= np_image.transpose ((2,0,1))
+    #np_image.transpose (1,2,0)
     return np_image
 
-
-def predict(image_tensor, model, device, cat_to_name, top_k):
+def predict(image_path, model, topkl):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
-
-    image_path: string. Path to image, directly to image and not to folder.
-    model: pytorch neural network.
-    top_k: integer. The top K classes to be calculated
-
-    returns top_probabilities(k), top_labels
     '''
-
-    # check top_k
-    if isinstance(top_k) == isinstance(None):
-        top_k = 5
-        print("Top K not specified, assuming K=5.")
-
-    # Set model to evaluate
-    model.eval()
-
-    # Convert image from numpy to torch
-    torch_image = torch.from_numpy(np.expand_dims(image_tensor, axis=0)).type(torch.FloatTensor)
-
-    model = model.cpu()
-
-    # Find probabilities (results) by passing through the function (note the log softmax means that its on a log scale)
-    log_probs = model.forward(torch_image)
-
-    # Convert to linear scale
-    linear_probs = torch.exp(log_probs)
-
-    # Find the top 5 results
-    top_probs, top_labels = linear_probs.topk(top_k)
-
-    # Detatch all of the details
-    top_probs = np.array(top_probs.detach())[0]  # This is not the correct way to do it but the correct way isnt working thanks to cpu/gpu issues so I don't care.
-    top_labels = np.array(top_labels.detach())[0]
-
-    # Convert to classes
-    idx_to_class = {val: key for key, val in model.class_to_idx.items()}
-    top_labels = [idx_to_class[lab] for lab in top_labels]
-    top_flowers = [cat_to_name[lab] for lab in top_labels]
-
-    return top_probs, top_labels, top_flowers
-
-
-def print_probability(probs, flowers):
-    """
-    Converts two lists into a dictionary to print on screen
-    """
-
-    for i, j in enumerate(zip(flowers, probs)):
-        print("Rank {}:".format(i + 1), "Flower: {}, liklihood: {}%".format(j[1], ceil(j[0] * 100)))
-
+    # TODO: Implement the code to predict the class from an image file
+    image = process_image (image_path) #loading image and processing it using above defined function
+    
+    #we cannot pass image to model.forward 'as is' as it is expecting tensor, not numpy array
+    #converting to tensor
+    im = torch.from_numpy (image).type (torch.FloatTensor)
+    
+    im = im.unsqueeze (dim = 0) #used to make size of torch as expected. as forward method is working with batches,
+    #doing that we will have batch size = 1 
+        
+    with torch.no_grad ():
+        output = model.forward (im)
+    output_prob = torch.exp (output) #converting into a probability
+    
+    probs, indeces = output_prob.topk (topkl)
+    probs = probs.numpy () #converting both to numpy array
+    indeces = indeces.numpy () 
+    
+    probs = probs.tolist () [0] #converting both to list
+    indeces = indeces.tolist () [0]
+    
+    
+    mapping = {val: key for key, val in
+                model.class_to_idx.items()
+                }
+    
+    classes = [mapping [item] for item in indeces]
+    #classes = pd.DataFrame ([mapping [item] for item in indeces]) #replacing indeces with classes
+    classes = np.array (classes) #converting to Numpy array 
+    
+    return probs, classes
 
 def main():
-    """
-    Executing relevant functions
-    """
+    model = model_verify
+    file_path = 'index.jpeg' #an example from test set
 
-    # Get Keyword Args for Prediction
-    args = arg_parser()
+    img = process_image (file_path)
 
-    # Load categories to names json file
-    with open(args.category_names, 'r') as f:
-        cat_to_name = json.load(f)
-
-    # Load model trained with train.py
-    model = load_checkpoint(args.checkpoint)
-
-    # Process Image
-    image_tensor = process_image(args.image)
-
-    # Use `processed_image` to predict the top K most likely classes
-    top_probs, top_labels, top_flowers = predict(image_tensor, model, cat_to_name, args.top_k)
-
-    # Print out probabilities
-    print_probability(top_flowers, top_probs)
-
+    probs, classes = predict (file_path, model, 5)
+    print(probs)
+    print(classes)
 
 if __name__ == '__main__':
     main()
