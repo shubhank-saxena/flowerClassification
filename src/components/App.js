f5d9b91e953f9994936af3828cce3450b01ab70a
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-20 00:48:26 +0530

    Add homepage

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Homepage from "./Homepage/Homepage";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Homepage />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./style.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <h1>flowerClassification using Deep Learning!</h1>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Homepage;
