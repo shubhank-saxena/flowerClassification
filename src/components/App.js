7cb524db09284ff4d18897f66d6b57a7e465b1a3
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-21 16:48:09 +0530

    Add center formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Homepage from "./Homepage/Homepage";
-
+import { Container } from "react-bootstrap";
+import "./style.css";
 class App extends React.Component {
   render() {
     return (
       <>
-        <Homepage />
+        <Container className="content">
+          <Homepage />
+        </Container>
       </>
     );
   }
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import "./style.css";
+import { Row } from "react-bootstrap";
 
 const Homepage = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <h1>flowerClassification using Deep Learning!</h1>
-        </Row>
-      </Container>
+      <Row>
+        <div className="content">
+          <h1>Flower Classification using Deep Learning</h1>
+          <h5>
+            This application is serves the purpose of identification of flower's
+            species from the picture that it is provided.
+          </h5>
+          <h5>
+            It's trained on Resnet model with base for feature extraction and
+            new layers added for classification system
+          </h5>
+
+          <h5>
+            It takes image as an input and displays <strong>top 5</strong>{" "}
+            classes with their respective probabilitites.
+          </h5>
+        </div>
+      </Row>
     </>
   );
 };
