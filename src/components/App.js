77fa5c5dd632b5f70283abe28f42755a6f9de2da
Shristi Gupta <68809622+shristigupta1408@users.noreply.github.com>
2020-10-03 13:35:19 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Homepage from "./Homepage/Homepage";
 import Prediction from "./Prediction/Prediction";
+import Image from "./Image/Image";
 import { Container } from "react-bootstrap";
 import "./style.css";
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <>
         <Container fluid className="content">
           <Homepage />
+          <Image />
           <Prediction />
         </Container>
       </>
