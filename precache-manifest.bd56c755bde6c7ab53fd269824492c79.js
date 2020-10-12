9ba8c918c93a30ef8b24372f35b29a949af05e19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-12 20:45:17 +0000

    Deploy shubhank-saxena/flowerClassification to github.com/shubhank-saxena/flowerClassification.git:gh-pages

---
+++ precache-manifest.bd56c755bde6c7ab53fd269824492c79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823a4cf4ad1ae5bb32ae39479cabb9f7",
+    "url": "/flowerClassification/index.html"
+  },
+  {
+    "revision": "115477bed9d122f5affe",
+    "url": "/flowerClassification/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "d12f3a84666bd15fa4c7",
+    "url": "/flowerClassification/static/css/main.c3ec641c.chunk.css"
+  },
+  {
+    "revision": "115477bed9d122f5affe",
+    "url": "/flowerClassification/static/js/2.b21a9659.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/flowerClassification/static/js/2.b21a9659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12f3a84666bd15fa4c7",
+    "url": "/flowerClassification/static/js/main.5d713662.chunk.js"
+  },
+  {
+    "revision": "fcd9d6d9b96af9e01bdc",
+    "url": "/flowerClassification/static/js/runtime-main.9e418d33.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/flowerClassification/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
